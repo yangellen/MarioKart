7bba67f0f5d9e9eb622bcbf253c309a297704832
Ellen Yang <yange@oregonstate.edu>
2021-05-22 23:46:22 -0700

    user can rotate a cart using a rotation gesture

--- MarioKart/ViewController.swift
+++ MarioKart/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
       // Do any additional setup after loading the view.
    }
 
-   //move to different location
+   //move kart to different location
    @IBAction func onPanGesture(_ panGesture: UIPanGestureRecognizer) {
       let location = panGesture.location(in: view)
 
@@ -24,12 +24,22 @@ class ViewController: UIViewController {
       kartView.center = location
    }
 
-   //change different size
+   //change kart to different size
    @IBAction func onPinchGesture(_ pinchGesture: UIPinchGestureRecognizer) {
       let scale = pinchGesture.scale
       //print("scale: \(scale)")
       let kartView = pinchGesture.view!
       kartView.transform = CGAffineTransform(scaleX: scale, y: scale)
    }
+
+   //rotate kart
+   @IBAction func onRotationGesture(_ rotationGesture: UIRotationGestureRecognizer) {
+      let rotation = rotationGesture.rotation
+      print("rotation: \(rotation)")
+      let kartView = rotationGesture.view!
+      kartView.transform = CGAffineTransform(rotationAngle: rotation)
+
+   }
+
 }
 

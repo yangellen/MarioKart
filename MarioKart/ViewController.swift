a9ef78cce1d668179213a83612972083015a5ebf
Ellen Yang <yange@oregonstate.edu>
2021-05-22 23:28:24 -0700

    user can adjust the size of a cart using a pinch gesture

--- MarioKart/ViewController.swift
+++ MarioKart/ViewController.swift
@@ -17,11 +17,18 @@ class ViewController: UIViewController {
    @IBAction func onPanGesture(_ panGesture: UIPanGestureRecognizer) {
       let location = panGesture.location(in: view)
 
-      print("Location: x: \(location.x), y: \(location.y)")
+     // print("Location: x: \(location.x), y: \(location.y)")
 
       let kartView = panGesture.view!
       kartView.center = location
    }
 
+   @IBAction func onPinchGesture(_ pinchGesture: UIPinchGestureRecognizer) {
+      let scale = pinchGesture.scale
+      print("scale: \(scale)")
+      let kartView = pinchGesture.view!
+      kartView.transform = CGAffineTransform(scaleX: scale, y: scale)
+
+   }
 }
 
